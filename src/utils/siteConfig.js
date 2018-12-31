66f8a00afcdefaed9545adc2c6651785f2d7c1f0
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2018-12-31 02:21:46 +0100

    First commit

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteTitle: 'e2p-jam-gcn',
+  siteTitleAlt: 'e2p-jam-gcn', // This allows an alternative site title for SEO schema.
+  publisher: 'e2p', // Organization name used for SEO schema
+  siteDescription:
+    'e2p-jam-gcn',
+  siteUrl: 'https://XXXXXX.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 6, // Number of posts shown on paginated pages
+  author: 'Sebastian Gelotte', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://github.com/sebastiangelotte', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@twitter', // Change for Twitter Cards
+  shortTitle: 'e2p-jam-gcn', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2018 e2p', // Copyright string for the RSS feed
+}
